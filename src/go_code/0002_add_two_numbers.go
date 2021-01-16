d51428fb0e2718883c8f62bc9c81854119e8f301
gsxhnd <gsxhnd@gmail.com>
2021-01-17 03:35:43 +0800

    feat: add two numbers

--- src/go_code/0002_add_two_numbers.go
+++ src/go_code/0002_add_two_numbers.go
@@ -13,5 +13,28 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	var carry int
+	var result = new(ListNode)
+	var resultCurr = result
+	for l1 != nil || l2 != nil || carry != 0 {
+		resultCurr.Next = new(ListNode)
+		resultCurr = resultCurr.Next
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		if carry != 0 {
+			resultCurr.Val += carry
+		}
+
+		resultCurr.Val = carry % 10
+		carry = carry / 10
+	}
+	return result.Next
 }
